730482223825deece6c5add714c1cf8f0f4435cf
Bloomfield, Keith (HAL Contractor) <KBloomfield@HollandAmerica.com>
2014-12-03 16:46:19 -0800

    Adding some tests for careers

--- src/main/java/pageobjects/CareersPage.java
+++ src/main/java/pageobjects/CareersPage.java
@@ -5,16 +5,52 @@ import org.openqa.selenium.WebDriver;
 
 public class CareersPage {
 
+    public static final String EXPECTED_HEADER_BUSINESS_INTELLIGENCE = "BUSINESS INTELLIGENCE";
+    public static final String EXPECTED_SECTION_HEADER_EXECUTIVE_MANAGEMENT = "EXECUTIVE MANAGEMENT";
+
+    private static final String URL_CAREERS = "http://www.doubledowninteractive.com/careers";
     private final WebDriver driver;
 
     private static final By BY_BODY_TAG = By.className("dd-page-careers");
+    private static final By BY_OPEN_POSITIONS_BUTTON = By.cssSelector("#dd-careers-page-scroll-trigger");
+    private static final By BY_BUSINESS_INTELLIGENCE = By.xpath("//*[@id=\"jvform\"]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td");
+    private static final By BY_EXECUTIVE_MANAGEMENT = By.xpath("//*[@id=\"jvform\"]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[5]/td");
+
 
     public CareersPage(WebDriver driver){
         this.driver = driver;
+    }
+
+    /**
+     * Short-circuit navigation to page under test. If the url is directly addressable,
+     * then we're wasting time walking to it from the home page.
+     */
+    public void navigateToCareers(){
+
+        driver.get(URL_CAREERS);
+        clickOpenPositions();
+
+        driver.switchTo().frame(driver.findElement(By.id("jobviteframe")));
     }
 
     public boolean isCareersPage(){
 
         return driver.findElement(BY_BODY_TAG).isDisplayed();
     }
+
+    public void clickOpenPositions(){
+
+        driver.findElement(BY_OPEN_POSITIONS_BUTTON).click();
+    }
+
+    public String getBusinessIntelligenceHeaderText() {
+
+        return driver.findElement(BY_BUSINESS_INTELLIGENCE).getText();
+    }
+
+    public String getExecutiveManagementHeaderText() {
+
+        return driver.findElement(BY_EXECUTIVE_MANAGEMENT).getText();
+    }
+
 }
---
+++ src/test/java/functionaltests/CareersPageOpenPositionsTest.java
@@ -0,0 +1,42 @@
+package functionaltests;
+
+import helpers.Scannable;
+import helpers.WebDriverHelper;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import pageobjects.CareersPage;
+
+import java.net.MalformedURLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Scannable
+public class CareersPageOpenPositionsTest {
+
+    private CareersPage careersPage;
+
+    @AfterClass
+    public static void tearDown(){
+        WebDriverHelper.getInstance().removeTest(CareersPageOpenPositionsTest.class);
+    }
+
+    @Before
+    public void init() throws MalformedURLException {
+
+        careersPage = new CareersPage(WebDriverHelper.getInstance().getWebDriver());
+        careersPage.navigateToCareers();
+    }
+
+    @Test
+    public void hiringForBusinessIntelligence(){
+
+        assertThat(careersPage.getBusinessIntelligenceHeaderText()).isEqualTo(careersPage.EXPECTED_HEADER_BUSINESS_INTELLIGENCE);
+    }
+
+    @Test
+    public void hiringForExecutiveManagement(){
+
+        assertThat(careersPage.getExecutiveManagementHeaderText()).isEqualTo(careersPage.EXPECTED_SECTION_HEADER_EXECUTIVE_MANAGEMENT);
+    }
+}
